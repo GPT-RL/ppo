cea9788d24f569c89d80553fb39a4d246c49b6f8
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2021-08-05 16:12:42 -0400

    use new Pluto version

--- dashboard/start.sh
+++ dashboard/start.sh
@@ -2,4 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 docker build -t pluto-dashboard . && \
-docker run --rm -it --mount "src=$SCRIPT_DIR/notebooks,dst=/workspace/notebooks,type=bind" -p 7777:7777 pluto-dashboard "$@"
+docker run --rm -it \
+    --mount "src=$SCRIPT_DIR/notebooks,dst=/workspace/notebooks,type=bind" \
+    --mount "src=$HOME/.cache/GPT/linguistic_analysis,dst=/workspace/linguistic_analysis,type=bind" \
+    -p 7777:7777 pluto-dashboard "$@"
