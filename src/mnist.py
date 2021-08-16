be0ad6e411c484c710680e9ebf5ce01b0814d9e7
Ethan Brooks <ethanabrooks@gmail.com>
2021-08-16 14:14:47 -0400

    add xor

--- src/mnist.py
+++ src/mnist.py
@@ -5,8 +5,9 @@ import os
 import time
 from pathlib import Path
 from pprint import pformat
-from typing import Literal, Optional
+from typing import List, Literal, Optional
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -15,7 +16,10 @@ import yaml
 from sweep_logger import HasuraLogger, Logger
 from tap import Tap
 from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import T_co
 from torchvision import datasets, transforms
+from torchvision.datasets import VisionDataset
 from transformers import GPT2Config, GPT2Model
 
 from spec import spec
@@ -42,6 +46,7 @@ class Args(Tap):
     config: Optional[str] = None
     cuda: bool = True
     dry_run: bool = False  # quickly check a single pass
+    dataset: str = Literal["mnist", "xor"]
     epochs: int = 14
     gamma: float = 0.7  # Learning rate step gamma (default: 0.7)
     gpt_size: Literal["small", "medium", "large", "xl"] = None
@@ -65,6 +70,8 @@ class Args(Tap):
 class GPTNet(nn.Module):
     def __init__(
         self,
+        input_shape: List[int],
+        num_outputs: int,
         gpt_size: str,
         randomize_parameters: bool,
         train_ln: bool,
@@ -95,35 +102,54 @@ class GPTNet(nn.Module):
             requires_grad = (train_wpe and "wpe" in name) or (train_ln and "ln" in name)
             p.requires_grad_(requires_grad)
         self.n_embd = self.gpt.config.n_embd
-        self.conv = (
-            nn.Conv2d(1, self.n_embd, 4, 4)
-            if one_layer
-            else nn.Sequential(
-                nn.Conv2d(1, 32, 3, 1),
-                nn.ReLU(),
-                nn.Conv2d(32, self.n_embd, 4, 4),
+        if len(input_shape) == 1:
+            input_size = input_shape[0]
+            self.net = (
+                nn.Linear(input_size, input_size * self.n_embd)
+                if one_layer
+                else nn.Sequential(
+                    nn.Linear(input_size, 32),
+                    nn.ReLU(),
+                    nn.Linear(32, input_size * self.n_embd),
+                )
             )
-        )
-        self.out = nn.Linear(self.gpt.config.n_embd, 10)
+        elif len(input_shape) == 3:
+            self.net = (
+                nn.Conv2d(1, self.n_embd, 4, 4)
+                if one_layer
+                else nn.Sequential(
+                    nn.Conv2d(1, 32, 3, 1),
+                    nn.ReLU(),
+                    nn.Conv2d(32, self.n_embd, 4, 4),
+                )
+            )
+        else:
+            raise InvalidDatasetError()
+        self.out = nn.Linear(self.gpt.config.n_embd, num_outputs)
 
     def forward(self, x):
-        x = self.conv(x)
+        x = self.net(x)
         x = x.reshape(x.size(0), self.n_embd, -1).transpose(2, 1)
         x = self.gpt(inputs_embeds=x).last_hidden_state[:, -1]
         x = self.out(x)
-        output = F.log_softmax(x, dim=1)
-        return output
+        return x
+        # output = F.log_softmax(x, dim=1)
+        # return output
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        input_shape: List[int],
+        num_outputs: int,
+    ):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(128, num_outputs)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -137,8 +163,31 @@ class Net(nn.Module):
         x = F.relu(x)
         x = self.dropout2(x)
         x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
+        return x
+        # output = F.log_softmax(x, dim=1)
+        # return output
+
+
+class XOR(VisionDataset):
+    def __init__(
+        self,
+        root: str,
+        seed: int,
+    ):
+        super().__init__(root)
+        random = np.random.RandomState(seed=seed)
+        self.inputs1 = random.choice(2, size=(len(self), 5))
+        self.inputs2 = random.choice(2, size=(len(self), 5))
+        self.targets = np.bitwise_xor(self.inputs1, self.inputs2).astype(np.float32)
+        self.inputs = np.concatenate([self.inputs1, self.inputs2], axis=-1).astype(
+            np.float32
+        )
+
+    def __len__(self) -> int:
+        return 70000
+
+    def __getitem__(self, index) -> T_co:
+        return self.inputs[index], self.targets[index]
 
 
 EPOCH = "epoch"
@@ -149,61 +198,8 @@ FPS = "fps"
 HOURS = "hours"
 
 
-def train(
-    args, model, device, train_loader, optimizer, epoch, start, logger: Optional[Logger]
-):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            log = {
-                EPOCH: epoch,
-                TRAIN_LOSS: loss.item(),
-                HOURS: (time.time() - start) / 3600,
-            }
-            logging.info(pformat(log))
-            if logger is not None:
-                log.update({"run ID": logger.run_id})
-                logger.log(log)
-            if args.dry_run:
-                break
-
-
-def test(model, device, test_loader, epoch, start, logger: Optional[Logger]):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(
-                output, target, reduction="sum"
-            ).item()  # sum up batch loss
-            pred = output.argmax(
-                dim=1, keepdim=True
-            )  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-
-    log = {
-        EPOCH: epoch,
-        TEST_LOSS: test_loss,
-        TEST_ACCURACY: 100.0 * correct / len(test_loader.dataset),
-        HOURS: (time.time() - start) / 3600,
-    }
-    if logger is not None:
-        log.update({"run ID": logger.run_id})
-        logger.log(log)
-    logging.info(pformat(log))
-    if logger is not None:
-        logger.log(log)
+class InvalidDatasetError(RuntimeError):
+    pass
 
 
 def main(args: Args):
@@ -271,14 +267,45 @@ def run(args, logger: Logger = None):
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
     )
-    dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
-    dataset2 = datasets.MNIST("../data", train=False, transform=transform)
+    if args.dataset == "mnist":
+        dataset1 = datasets.MNIST(
+            "../data", train=True, download=True, transform=transform
+        )
+        dataset2 = datasets.MNIST("../data", train=False, transform=transform)
+        num_outputs = 10
+
+        def get_loss(out, tgt, reduction="mean"):
+            return F.nll_loss(F.log_softmax(out, dim=1), tgt, reduction=reduction)
+
+        def prediction(out):
+            return F.log_softmax(out, dim=1).argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
+
+    elif args.dataset == "xor":
+        dataset = XOR("../data", args.seed)
+        dataset1, dataset2 = torch.utils.data.random_split(dataset, [60000, 10000])
+        num_outputs = 5
+
+        def get_loss(out, tgt, reduction="mean"):
+            return F.mse_loss(torch.sigmoid(out), tgt, reduction=reduction)
+
+        def prediction(out):
+            return torch.sigmoid(out).round()
+
+    else:
+        raise InvalidDatasetError()
+
     train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+    sample_input, _ = dataset1[0]
+
     model = (
-        Net()
+        Net(input_shape=sample_input.shape, num_outputs=num_outputs)
         if args.gpt_size is None
         else GPTNet(
+            input_shape=sample_input.shape,
+            num_outputs=num_outputs,
             gpt_size=args.gpt_size,
             randomize_parameters=args.randomize_parameters,
             train_ln=args.train_ln,
@@ -289,9 +316,56 @@ def run(args, logger: Logger = None):
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     start = time.time()
+
     for epoch in range(1, args.epochs + 1):
-        test(model, device, test_loader, epoch, start, logger)
-        train(args, model, device, train_loader, optimizer, epoch, start, logger)
+        model.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                test_loss += get_loss(
+                    output, target, reduction="sum"
+                ).item()  # sum up batch loss
+                pred = prediction(output)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)
+
+        log = {
+            EPOCH: epoch,
+            TEST_LOSS: test_loss,
+            TEST_ACCURACY: 100.0 * correct / len(test_loader.dataset),
+            HOURS: (time.time() - start) / 3600,
+        }
+        if logger is not None:
+            log.update({"run ID": logger.run_id})
+            logger.log(log)
+        logging.info(pformat(log))
+        if logger is not None:
+            logger.log(log)
+
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = get_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % args.log_interval == 0:
+                log = {
+                    EPOCH: epoch,
+                    TRAIN_LOSS: loss.item(),
+                    HOURS: (time.time() - start) / 3600,
+                }
+                logging.info(pformat(log))
+                if logger is not None:
+                    log.update({"run ID": logger.run_id})
+                    logger.log(log)
+                if args.dry_run:
+                    break
         scheduler.step()
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
