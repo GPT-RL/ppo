7bf0739c74529c09096749724a6c09151dcb1186
Ethan Brooks <ethanabrooks@gmail.com>
2021-08-26 21:12:31 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 mkdir -p logs ~/.cache/GPT ~/.cache/huggingface
-name=gpt
+name=ppo_agent
 docker build -t $name .
 docker run --rm -it\
   --env-file .env\
-  --gpus all\
+  --gpus "$1"\
   -e HOST_MACHINE="$(hostname -s)"\
   -v "$(pwd)/logs:/tmp/logs"\
   -v "$HOME/.cache/GPT/:/root/.cache/GPT" \
   -v "$HOME/.cache/huggingface/:/root/.cache/huggingface" \
-  $name "$@"
+  $name "${@:2}"
