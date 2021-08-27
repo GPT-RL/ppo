e813b0a9c5cf96c19752decc8482eecaf90a5c5d
Ethan Brooks <ethanabrooks@gmail.com>
2021-08-26 20:52:35 -0400

    go back to minimal train set

--- src/babyai_env.py
+++ src/babyai_env.py
@@ -47,8 +47,7 @@ def get_train_and_test_objects():
         # np.random.shuffle(remaining)
         # yield from remaining
 
-    # train_objects = [*pairs()][:1]
-    train_objects = [("ball", "red")]
+    train_objects = [*pairs()]
     test_objects = [x for x in all_object_types() if x not in set(train_objects)]
     return test_objects, train_objects
 
